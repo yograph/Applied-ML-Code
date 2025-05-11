7bdf44b1d22acd0617d4514024b310bbf3eb1b4e
yograph <youseffarahat7@gmail.com>
2025-05-11 09:47:39 +0200

    ALmost finished creating the YOLOX preprocessing and put the first metric

--- Cancer_Detection/data/YOLOX_training/YOLOX_classes.py
+++ Cancer_Detection/data/YOLOX_training/YOLOX_classes.py
@@ -1,24 +1,15 @@
 """
-This is to train the YOLOX
+This is for the dependencies of the yolox
 """
 KAGGLE = True
 
+!git clone https://github.com/roboflow-ai/YOLOX.git %cd YOLOX
+!pip3 install -U pip && pip3 install -r requirements.txt
+!pip3 install -v -e . 
+!pip uninstall -y torch torchvision torchaudio # May need to change in the future if Colab no longer uses CUDA 11.0 !pip install torch==1.7.1+cu110 torchvision==0.8.2+cu110 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
+%cd /content/
+!git clone https://github.com/NVIDIA/apex
+%cd apex
+!pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
+!pip3 install cython; pip3 install 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI'
 
-class train_yolox:
-    """
-    THis is to train a yolox
-    
-    """
-    OUTPUT_RECT_DIR  = "../Cancer_Detection/data/dataset_images/rsna-mammography-images-as-pngs/images_as_pngs_1024/train_images_processed_1024/*/*"
-    OUTPUT_FINAL_DIR = "../Cancer_Detection/data/dataset_images/rsna-mammography-images-as-pngs/images_as_pngs_1024/train_images_processed_1024/*/*"
-
-    def __init__(self):
-        if KAGGLE:
-            pattern = "../input/rsna-mammography-images-as-pngs/images_as_pngs_1024/train_images_processed_1024/*/*"
-        else:
-            pattern = "../Cancer_Detection/data/dataset_images/rsna-mammography-images-as-pngs/images_as_pngs_1024/train_images_processed_1024/*/*"
-        main = []
-
-
-
-# class 
